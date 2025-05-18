d63ebb91a604703906b978704eb1e005584e2d9f
BaTien <tienriven@gmail.com>
2025-05-18 22:19:23 +0700

    sửa lần 3

--- NodeJS/src/config/connectDB.js
+++ NodeJS/src/config/connectDB.js
@@ -2,7 +2,8 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('toilatien', 'root', null, {
   host: 'localhost',
-  dialect: 'mysql'
+  dialect: 'mysql',
+  logging: false,
 });
 
 let connectDB = async () => {
@@ -13,3 +14,5 @@ let connectDB = async () => {
     console.error('Unable to connect to the database:', error);
   }
 }
+
+module.exports = connectDB;
--- NodeJS/src/server.js
+++ NodeJS/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from './route/web';
+import connectDB from './config/connectDB';
 require('dotenv').config();
 
 let app = express();
@@ -14,7 +15,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 viewEngine(app);
 initWebRoutes(app);
 
-let port = process.env.PORT || 6969;
+connectDB();
+
+let port = process.env.PORT || 8080;
 //Port === undefined => port = 6969
 
 app.listen(port, () => {
