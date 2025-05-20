6e82e7d4a6835cd9f08a786adcf65d9d19132db8
BaTien <tienriven@gmail.com>
2025-05-20 15:53:08 +0700

    Next

---
+++ NodeJS/src/services/userService.js
@@ -0,0 +1,64 @@
+import db from "../models/index.js";
+import bcrypt from "bcryptjs";
+
+let handleUserLogin =  (email, password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let userData = {};
+            let isExist = await checkUserEmail(email);
+            if (isExist) {
+
+                let user = await db.User.findOne({
+                    attributes: ['email', 'roleId', 'password'],
+                    where: { email: email },
+                    raw: true,
+                });
+
+                if (user) {
+                    let check = await bcrypt.compareSync(password, user.password);
+                    if (check) {
+                        userData.errCode = 0;
+                        userData.message = 'Login success';
+                        delete user.password;
+                        userData.user = user;
+                    } else {
+                        userData.errCode = 3;
+                        userData.message = 'Wrong password';
+                    }
+
+                } else {
+                    userData.errCode = 2;
+                    userData.message = 'User not found';
+                }
+            } else {
+                userData.errCode = 1;
+                userData.errMessage = 'Your email is not exist in your system. Please try other email';
+            }
+
+            resolve(userData);
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
+let checkUserEmail = (email) => {   
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { email: userEmail }
+            });
+            if (user) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+        } catch (e) {
+            reject(e);
+        }
+    });
+}
+
+module.exports = {
+    handleUserLogin: handleUserLogin
+}
